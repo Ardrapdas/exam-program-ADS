77f292b37dc9e8026bb1cc3d081d003cd2fe1000
Ardrapdas <94525953+Ardrapdas@users.noreply.github.com>
2022-03-09 12:37:47 +0530

    Add files via upload

---
+++ prgm1.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+struct node;
+{
+struct node*newnode
+}; int*top;
+void main();
+
+{
+void push(int);
+void pop();
+void disply();
+void exit();
+{
+int choice,value;
+}
+while(1)
+
+{
+    printf("***MENU***");
+    printf("\n 1.push \n2.pop \n3.disply \n4.exit");
+    {
+        case 1 = printf("\n enter the element to insert \n");
+                scanf("%d",&);
+                push(int);
+        case 2 =pop();
+        case 3 =disply();
+        case 4 = exit();
+        {
+        printf("\n enter your choice:\n");
+        scanf("%d",&choice);
+        choice(break);
+       }
+    }
+}
+void push(int);
+{
+struct node*newnode;
+ struct node = malloc(size(struct newnode));
+{
+if (top==NULL)
+    newnode->next=NULL;
+else
+   (top->next=top);
+}
+void pop();
+{
+if (top==NULL)
+ printf("\n data cannot be retrieved\n");
+else
+   (top=top->next);
+}
+printf("\n data retrieved successfully");
+}
+}
+
+
+
+
+
+
+
